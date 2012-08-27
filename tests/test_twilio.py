e9981fa657caecd847f7e1cfbadd5cdb95b21685
Rob Spectre <rob.spectre@gmail.com>
2012-08-27 15:53:48 -0400

    Beefed up parameters of Twilio test methods.

--- tests/test_twilio.py
+++ tests/test_twilio.py
@@ -1,6 +1,12 @@
 import unittest
 from .context import app
 
+
+app.config['TWILIO_ACCOUNT_SID'] = 'ACxxxxxx'                                     
+app.config['TWILIO_AUTH_TOKEN'] = 'yyyyyyyyy'                                     
+app.config['TWILIO_CALLER_ID'] = '+15558675309'
+
+
 class TwiMLTest(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
@@ -12,26 +18,33 @@ class TwiMLTest(unittest.TestCase):
                 "</Response>: %s" % response.data)
         self.assertEqual("200 OK", response.status)
 
-    def sms(self, body, path='/sms', number='+15555555555'):
+    def sms(self, body, path='/sms', to=app.config['TWILIO_CALLER_ID'],
+            from_='+15558675309'):
         params = {
             'SmsSid': 'SMtesting',
-            'AccountSid': 'ACtesting',
-            'From': number,
-            'To': '+16666666666',
+            'AccountSid': app.config['TWILIO_ACCOUNT_SID'],
+            'To': to, 
+            'From': from_,
             'Body': body,
-            'ApiVersion': '2010-04-01',
-            'Direction': 'inbound'}
+            'FromCity': 'BROOKLYN',
+            'FromState': 'NY',
+            'FromCountry': 'US',
+            'FromZip': '55555'}
         return self.app.post(path, data=params)
 
-    def call(self, path='/voice', number='+15555555555', digits=None):
+    def call(self, path='/voice', to=app.config['TWILIO_CALLER_ID'],
+            from_='+15558675309', digits=None):
         params = {
             'CallSid': 'CAtesting',
-            'AccountSid': 'ACtesting',
-            'From': number,
-            'To': '+16666666666',
+            'AccountSid': app.config['TWILIO_ACCOUNT_SID'],
+            'To': to,
+            'From': from_,
             'CallStatus': 'ringing',
-            'ApiVersion': '2010-04-01',
-            'Direction': 'inbound'}
+            'Direction': 'inbound',
+            'FromCity': 'BROOKLYN',
+            'FromState': 'NY',
+            'FromCountry': 'US',
+            'FromZip': '55555'}
         if digits:
             params['Digits'] = digits
         return self.app.post(path, data=params)

e394553adb7ec0cad589f213054d598d32b38ee6
Rob Spectre <rob.spectre@gmail.com>
2014-09-13 23:36:16 -0400

    Attempt to kick coveralls.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from hackpack.app import app
 
-# If PORT not specified by environment, assume development config.
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     if port == 5000:
